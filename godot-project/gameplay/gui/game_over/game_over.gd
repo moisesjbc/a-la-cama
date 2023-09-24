9c0d98c6fe1433e8f69c9366a75ea2f3f19ba101
Moisés J <moisesjbc@gmail.com>
2023-09-24 12:55:54 +0100

    Add main menu button to game over menu

--- godot-project/gameplay/gui/game_over/game_over.gd
+++ godot-project/gameplay/gui/game_over/game_over.gd
@@ -19,3 +19,8 @@ func _on_restart_button_pressed():
 func _input(event):
 	if visible and event is InputEventKey and not event.pressed and event.scancode == KEY_SPACE:
 		_on_restart_button_pressed()
+
+
+func _on_main_menu_button_pressed():
+	stop()
+	get_tree().change_scene("res://gui/menus/main_menu/main_menu.tscn")
